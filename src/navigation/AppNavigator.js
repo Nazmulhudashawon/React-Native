7de3b808e79914f5696c87708c9515d353e470b3
Nazmulhudashawon <shawonnazmulhuda@gmail.com>
2021-12-18 20:14:53 +0600

    first commit

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { AsyncStorage, YellowBox } from 'react-native';
+import { useSelector } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { navigationRef } from './RootNavigation';
+import { DrawerNavigator, IntroStackScreen } from './StoneNavigator';
+import { useDispatch } from 'react-redux';
+import { Logout } from '../reducers';
+import { Host } from 'react-native-portalize';
+import { urlRedirect } from '../utils/Tools';
+import * as Linking from 'expo-linking';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+
+export const AppNavigator = () => {
+  const [value, setValue] = useState(null);
+  const dispatch = useDispatch();
+  const isFirstOpen = useSelector((state) => state.store.isFirstOpen);
+  useEffect(() => {
+    Linking.addEventListener(
+      'url',
+      (event) => {
+        urlRedirect(event.url);
+      },
+      [urlRedirect],
+    );
+    Linking.getInitialURL().then(urlRedirect);
+    Linking.removeEventListener(
+      'url',
+      (event) => {
+        urlRedirect(event.url);
+      },
+      [urlRedirect],
+    );
+  }, [urlRedirect]);
+
+  useEffect(() => {
+    const isFirstTime = async () => {
+      const firstOpen = await AsyncStorage.getItem('isFirstTime');
+      setValue(firstOpen);
+    };
+    isFirstTime();
+    const autoLogout = async () => {
+      const getUser = await AsyncStorage.getItem('user');
+      if (getUser) {
+        const user = await JSON.parse(getUser);
+        if (user.data.expireTime - Date.now() < 0) {
+          dispatch(Logout());
+        }
+      }
+      return;
+    };
+    autoLogout();
+  }, []);
+  useEffect(() => {
+    const autoLogout = async () => {
+      const getUser = await AsyncStorage.getItem('user');
+      if (getUser) {
+        const user = await JSON.parse(getUser);
+        if (user.data.expireTime - Date.now() < 0) {
+          dispatch(Logout());
+        }
+      }
+      return;
+    };
+    autoLogout();
+  }, []);
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <Host>
+        {(isFirstOpen || value !== null) && <DrawerNavigator />}
+        {!isFirstOpen && value === null && <IntroStackScreen />}
+      </Host>
+    </NavigationContainer>
+  );
+};
